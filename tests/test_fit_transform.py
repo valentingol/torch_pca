1d08d3e290d735c2b5a927d56aac854321e8c333
Valentin Goldité <valentin.goldite@gmail.com>
2024-06-17 19:13:19 +0200

    ✅ Cover covariance eigh and auto solver

--- tests/test_fit_transform.py
+++ tests/test_fit_transform.py
@@ -9,10 +9,13 @@ from sklearn.decomposition import PCA as PCA_sklearn
 from torch_pca import PCA
 
 
-def test_basic() -> None:
-    """Basic tests."""
+def test_fullsvd() -> None:
+    """Test basic full SVD."""
     input_1 = torch.load("tests/input_data.pt").to(torch.float32) + 2.0
-    torch_model = PCA(n_components=2).fit(input_1)
+    torch_model = PCA(
+        n_components=2,
+        svd_solver="full",
+    ).fit(input_1)
     sklearn_model = PCA_sklearn(
         n_components=2,
         svd_solver="full",
@@ -76,3 +79,46 @@ def test_centering() -> None:
     # Unkown centering
     with pytest.raises(ValueError, match="Unknown centering.*"):
         model.transform(inputs_1, center="UNKNOWN")
+
+
+def test_covariance_eigh() -> None:
+    """Test SVD based on covariance matrix."""
+    input_1 = torch.load("tests/input_data.pt").to(torch.float32) + 2.0
+    torch_model = PCA(n_components=2, svd_solver="covariance_eigh").fit(input_1)
+    sklearn_model = PCA_sklearn(
+        n_components=2,
+        svd_solver="covariance_eigh",
+        whiten=False,
+    ).fit(input_1)
+    for attr_name in ["components_", "explained_variance_", "singular_values_"]:
+        attr_torch = getattr(torch_model, attr_name)
+        attr_sklearn = getattr(sklearn_model, attr_name)
+        check.is_true(
+            torch.allclose(
+                attr_torch,
+                torch.tensor(attr_sklearn, dtype=torch.float32),
+                rtol=1e-5,
+                atol=1e-5,
+            ),
+            f"Failed for attribute {attr_name}",
+        )
+
+
+def test_fail_svd() -> None:
+    """Test unknown SVD solver."""
+    input_1 = torch.randn(200, 10)
+    with pytest.raises(ValueError, match="Unknown SVD solver.*"):
+        PCA(n_components=2, svd_solver="UNKNOWN").fit(input_1)
+
+
+def test_auto_svd() -> None:
+    """Test auto SVD solver."""
+    input_1 = torch.randn(200, 10)
+    model = PCA(n_components=2).fit(input_1)
+    check.equal(model.svd_solver_, "covariance_eigh")
+    input_2 = torch.randn(50, 8)
+    model = PCA(n_components=2).fit(input_2)
+    check.equal(model.svd_solver_, "full")
+    input_3 = torch.randn(10, 1200)
+    model = PCA(n_components=2).fit(input_3)
+    check.equal(model.svd_solver_, "full")
